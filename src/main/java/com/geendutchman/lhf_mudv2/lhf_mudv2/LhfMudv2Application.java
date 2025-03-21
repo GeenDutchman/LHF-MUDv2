cddcfc7f7f65ba0677c106860f10ada3a3df8237
Drake Foltz <foltz.dw@gmail.com>
2025-03-20 22:41:18 -0600

    0.0.1
    Initial generated

---
+++ src/main/java/com/geendutchman/lhf_mudv2/lhf_mudv2/LhfMudv2Application.java
@@ -0,0 +1,13 @@
+package com.geendutchman.lhf_mudv2.lhf_mudv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhfMudv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhfMudv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/geendutchman/lhf_mudv2/lhf_mudv2/LhfMudv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geendutchman.lhf_mudv2.lhf_mudv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhfMudv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
